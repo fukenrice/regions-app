968886ab8810d6a0d378c42d778c3f1629e887dc
fukenrice <63079551+fukenrice@users.noreply.github.com>
2023-09-28 23:03:12 +0300

    Add data layer

--- app/src/main/java/com/example/regions_app/data/api/ApiService.kt
+++ app/src/main/java/com/example/regions_app/data/api/ApiService.kt
@@ -1,7 +1,9 @@
 package com.example.regions_app.data.api
 
+import com.example.regions_app.data.model.RegionModel
 import retrofit2.http.GET
 
 interface ApiService {
-
+    @GET("getBrands")
+    suspend fun getRegions(): RegionModel
 }
